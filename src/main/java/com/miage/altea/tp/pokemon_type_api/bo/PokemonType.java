3d28a80e9457f6e3b80e418f4a0ece6e8c63f43f
Mathieu Sansen <mathieu.sansen.etu@univ-lille.fr>
2020-01-20 12:16:38 +0100

    First commit

---
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/bo/PokemonType.java
@@ -0,0 +1,39 @@
+package com.miage.altea.tp.pokemon_type_api.bo;
+
+import java.util.List;
+
+public class PokemonType {
+
+    private int id;
+    private int baseExperience;
+    private int height;
+    private String name;
+    private Sprites sprites;
+    private Stats stats;
+    private int weight;
+    private List<String> types;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Sprites getSprites() {
+        return sprites;
+    }
+
+    public void setSprites(Sprites sprites) {
+        this.sprites = sprites;
+    }
+}
