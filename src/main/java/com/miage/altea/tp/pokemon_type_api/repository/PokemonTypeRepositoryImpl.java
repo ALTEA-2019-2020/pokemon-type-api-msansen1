9cf738566653ef7ac50a27ec3c94cbf384b089c0
Mathieu <msansen@gmail.com>
2020-01-27 09:04:15 +0100

    DOING A la partie 7

--- src/main/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonTypeRepositoryImpl.java
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/repository/PokemonTypeRepositoryImpl.java
@@ -2,11 +2,14 @@ package com.miage.altea.tp.pokemon_type_api.repository;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.miage.altea.tp.pokemon_type_api.bo.PokemonType;
+import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Repository
 public class PokemonTypeRepositoryImpl implements PokemonTypeRepository {
 
     private List<PokemonType> pokemons;
@@ -26,19 +29,17 @@ public class PokemonTypeRepositoryImpl implements PokemonTypeRepository {
     @Override
     public PokemonType findPokemonTypeById(int id) {
         System.out.println("Loading Pokemon information for Pokemon id " + id);
-
-        // TODO
+        return this.pokemons.stream().filter(it -> it.getId() == id).findAny().orElseThrow();
     }
 
     @Override
     public PokemonType findPokemonTypeByName(String name) {
         System.out.println("Loading Pokemon information for Pokemon name " + name);
-
-        // TODO
+        return this.pokemons.stream().filter(it -> it.getName().equals(name)).findAny().orElseThrow();
     }
 
     @Override
     public List<PokemonType> findAllPokemonType() {
-        // TODO
+        return this.pokemons.stream().sorted((o1, o2) -> o2.getId() - o1.getId()).collect(Collectors.toList());
     }
 }
